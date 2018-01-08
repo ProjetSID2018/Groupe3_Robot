203fbe4fd4de1b4fca3398206c295bca465d7ca6
MorganSeguela <morgan.seguela@gmail.com>
2018-01-08 10:31:32 +0100

    rss_gorafi
    lecture_Rss_gorafi

---
+++ rss_gorafi.py
@@ -0,0 +1,18 @@
+import os
+import lxml.html as lh
+import json
+import datetime as date
+from bs4 import BeautifulSoup
+import requests
+
+url_rss_gorafi = "http://www.legorafi.fr/feed/"
+
+req = requests.get(url_rss_gorafi)
+data = req.text
+
+soup = BeautifulSoup(data)
+
+print(soup.prettify())
+
+
+
