8ebe3beba15fec2ab56b7146ce0d6c1cdbffaeee
MorganSeguela <morgan.seguela@gmail.com>
2018-01-08 15:16:50 +0100

    final_rss_gorafi
    analyse du flux rss gorafi

--- rss_gorafi.py
+++ rss_gorafi.py
@@ -4,15 +4,18 @@ import json
 import datetime as date
 from bs4 import BeautifulSoup
 import requests
+import re
 
 url_rss_gorafi = "http://www.legorafi.fr/feed/"
 
+
 req = requests.get(url_rss_gorafi)
 data = req.text
+print('ok')
 
-soup = BeautifulSoup(data)
+soup = BeautifulSoup(data, "lxml")
 
 items = soup.find_all("item")
-print(items)
-
+for item in items:
+    print(re.search(r"<link/>(.*)", str(item))[1])
 
