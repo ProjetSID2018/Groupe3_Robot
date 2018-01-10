654802173e24f9e5dcb26e32b99460e956a9d1ee
BENJEBRIA Sofian <sofian.benjebria@hotmail.fr>
2018-01-10 10:45:43 +0100

    Modification code gorafi
    Modification code gorafi + replacement du code create_json

--- g4_gorafi.py
+++ g4_gorafi.py
@@ -1,28 +1,29 @@
 # Groupe 4
-# Réalisé par BENJEBRIA Sofian, DELOEUVRE Noémie, SEGUELA Morgan
+# Realized by BENJEBRIA Sofian, DELOEUVRE Noémie, SEGUELA Morgan
 
 import os
-import json
 import datetime as date
 from bs4 import BeautifulSoup
 import requests
 import re
+from G4_create_json import create_json
 
-# Chemin à modifier : cible où l'on va stocker les fichier json
-file_target = "/var/www/html/projet2018/data/clean/robot/" + str(date.datetime.now().date()) +"/"
+# Path to change : target where we will store the json files
+file_target="/Users/Sofian/Documents/Robot/" + str(date.datetime.now().date()) +"/"
+#file_target = "/var/www/html/projet2018/data/clean/robot/" + str(date.datetime.now().date()) +"/"
 os.makedirs(file_target, exist_ok=True)
 
 url_rss_gorafi = "http://www.legorafi.fr/feed/"
 
-# On récupère les flux URL pour chaque nouvel article
-# Chaque article codé en HTML est analysé avec beautiful soup.
+# We retrieve the URL feeds for each new article
+# Each HTML-coded article is analyzed with beautiful soup
 req = requests.get(url_rss_gorafi)
 data = req.text
 soup = BeautifulSoup(data, "lxml")
 items = soup.find_all("item")
 article_gorafi = []
 
-# On récupère tous les nouveaux articles dans une liste
+# We're picking up every new item in a list
 for item in items:
     article_gorafi.append(re.search(r"<link/>(.*)", str(item))[1])
 file_json = []
@@ -40,14 +41,14 @@ for article in article_gorafi:
 
     author = []
 
-    # Récupération de l'auteur et de la date de publication
+    # Recovery of author and publication date
     for span in soup.find_all('span'):
         if span.get("class") == ['context']:
             author.append(span.a.get_text())
             for valeur in re.finditer('[0-9]{2}\/[0-9]{2}\/[0-9]{4}', str(span)):
                 date_p = valeur.group(0)
 
-    # Récupération du thème
+    # Retrieving the theme
     for ul in soup.find_all('ul'):
         if ul.get("class") == ['post-categories']:
             for li in ul.find_all('li'):
@@ -68,19 +69,16 @@ for article in article_gorafi:
             "theme": theme,
             "content": contents
     }
-    file_json.append(new_article)
+    if theme != "Magazine":
+        file_json.append(new_article)
 
-sources = "legorafi"
+sources = "legorafi/"
 cur_date = date.datetime.now().date()
 
 if not os.path.exists(file_target+sources):
     os.makedirs(file_target+sources)
 
-i = 1
-# Chaque article est exporté en format json et nommé de la forme suivante :
-# art_lg_numero_datejour_robot.json
-for article in file_json:
-    file_art = file_target + sources + "art_lg_" + str(i) + "_" + str(cur_date) + "_robot.json"
-    with open(file_art, "w", encoding="UTF-8") as fic:
-        json.dump(article, fic, ensure_ascii=False)
-    i += 1
+
+# Call the create_json function
+create_json(file_target, file_json, sources, "lg")
+
