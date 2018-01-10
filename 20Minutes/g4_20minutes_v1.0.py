eb1ef29e58747b326eee0e1e4f0e5dc2897f026a
Etudiant <etudiant@debian>
2018-01-10 16:48:02 +0100

    decoupage du code

--- 20Minutes/g4_20minutes_v1.0.py
+++ 20Minutes/g4_20minutes_v1.0.py
@@ -4,11 +4,8 @@ import os
 import sys
 import json
 import datetime as date
-from bs4 import BeautifulSoup
 from unidecode import unidecode
-import requests
 import re
-sys.path.append('~/./G4_create_json')
 
 # Verifier si le tag contient le texte Copyright
 def has_copyright(tag):
@@ -17,14 +14,60 @@ def has_copyright(tag):
     """
     return "Copyright" in tag.get_text()
 
+# Prend en parametre une adresse url et retour la soup
+def get_soup(url):
+    import requests
+    from bs4 import BeautifulSoup
+    req = requests.get(url)
+    soup = BeautifulSoup(req.text, "html.parser")
+    return soup
+
+"""Prend en argument une adresse url (url) et retourne une article au format 
+    {
+        "title" : str,
+        "newspaper" : str,
+        "author" : [str],
+        "date_publi" : str,
+        "content" : str,
+        "theme" : str
+        }
+"""
+def get_article(url):
+    soup=get_soup(url)
+    article=soup.find("article")
+    # Titre de l'article
+    title=article.find("h1").get_text()
+    # tableau vide quand il y'a pas d'autheur sinon tableau de(s) auteur(s)
+    authors= [] if article.find("header").find("p",class_="authorsign-label")==None else unidecode(article.find("header").find("p",class_="authorsign-label").get_text()).split(" et ")
+    # Date de publication de l'article
+    date_pub=article.find("time").get("datetime")
+    # Theme de l'article
+    theme=article.find("ol",class_="breadcrumb-list").find_all("li")[1].find("span").get_text()
+    # Contenu de l'article
+    content=""
+    for p in article.find("div",class_="content").find_all("p"):
+        content=content+p.get_text()
+    # Nom du journal
+    newspaper=soup.find("footer").find(has_copyright).find("a").get_text()
+    regex = re.compile(r'[\n\r\t]')
+    # Elever les \n \r \t du contenu
+    content = regex.sub("", content)
+    return {
+        "title" : unidecode(title),
+        "newspaper" : unidecode(newspaper),
+        "author" : authors,
+        "date_publi" : date_pub,
+        "content" : unidecode(content),
+        "theme" : unidecode(theme)
+        }
+
+
 # Chemin repertoire des articles
-fileTarget="/home/etudiant/Documents/ProjetSID/Groupe4_Robot/20Minute/Art/"
+fileTarget="/home/etudiant/Documents/ProjetSID/Groupe4_Robot/20Minutes/Art/"
 
 url_rss= "http://www.20minutes.fr/feeds/rss-actu-france.xml"
 
-req = requests.get(url_rss)
-
-soup = BeautifulSoup(req.text, "html.parser")
+soup = get_soup(url_rss)
 
 items = soup.find_all("item")
 
@@ -33,53 +76,16 @@ articles=[]
 for item in items:
     #Récuperer le lien des articles
     url=re.search(r"<link/>(.*)<pubdate>", str(item)).group(1)
-    req = requests.get(url)
-    data = req.text
-    soup=BeautifulSoup(data,"html.parser")
-    article=soup.find("article",id="main-content")
+    articles.append(get_article(url))
 
-    # Titre de l'article
-    title=article.find("header").find("h1").get_text()
-    # tableau vide quand il y'a pas d'autheur sinon tableau de(s) auteur(s)
-    authors= [] if article.find("header").find("p",class_="authorsign-label")==None else unidecode(article.find("header").find("p",class_="authorsign-label").get_text()).split(" et ")
-    # Date de publication de l'article
-    date_pub=article.find("header").find("time").get("datetime")
-    # Theme de l'article
-    theme=article.find("ol",class_="breadcrumb-list").find_all("li")[1].find("span").get_text()
-    # Contenu de l'article
-    content=""
-    for p in article.find("div",class_=["lt-endor-body", "content"]).find_all("p"):
-        content=content+p.get_text()
-
-    regex = re.compile(r'[\n\r\t]')
-    # Elever les \n \r \t du contenu
-    content = regex.sub("", content)
-
-    # Nom du journal
-    newspaper=soup.find("footer").find(has_copyright).find("a").get_text()
-
-    # Date courrent
-    now=str(date.datetime.now().date())
-
-    # Insérer le nouveau article dans un le tableau
-    articles.append({
-    "title" : unidecode(title),
-    "newspaper" : unidecode(newspaper),
-    "author" : authors,
-    "date_publi" : date_pub,
-    "content" : unidecode(content),
-    "theme" : unidecode(theme)
-    })
-
-
-sources = "20minutes/"
 
 # Creer le repertoire s'il n'existe pas
-if not os.path.exists(fileTarget+sources):
-    os.makedirs(fileTarget+sources)
-
+if not os.path.exists(fileTarget):
+    os.makedirs(fileTarget)
+# Date courrent
+now=str(date.datetime.now().date())
 # Enumérer les articles et pour chaque article , copier le contenu dans un fichier de nom art_numAr_date_robot.json
 for i,article in enumerate(articles):
-    file_art = fileTarget + sources + "art_"+str(i)+"_"+ now + "_robot.json"
+    file_art = fileTarget  + "art_"+str(i)+"_"+ now + "_robot.json"
     with open(file_art, "w", encoding="UTF-8") as fil:
         json.dump(article, fil,indent=4)
