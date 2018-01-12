de2d3a08de2f7c1509af460112341ca7b988145b
MorganSeguela <morgan.seguela@gmail.com>
2018-01-12 01:02:16 +0100

    server ready fold
    folder that contains everything for the server
    parserV1.py : that call python files
    liberation - lemonde - futurascience
    g4_utils_v31.py
    
    You have to launch it alone first

---
+++ server_ready_files/g4_futurasciences_v1.py
@@ -0,0 +1,88 @@
+﻿""" -*- coding: utf-8 -*-
+ Groupe 4
+ MOTHES Céline
+ HERVE Pierrick
+ V1 : create code
+ V1.1 : create function
+"""
+import unidecode
+import re
+import g4_utils_v31 as utils
+import datetime as date
+
+
+def recovery_information_fusc(url):
+    """
+        Arguments:
+            url : string
+        Return :
+            article : dictionary
+        It retrieve for each article the title, newspaper, author, date, theme
+    """
+    soup = utils.recovery_flux_urss(url)
+    # retrieve title
+    title = unidecode.unidecode(soup.title.string)
+    indice = title.find('|')
+    if indice != -1:
+        title = title[:indice-1]
+
+    # retrieve the author
+    author = []
+    for h3 in soup.find_all('h3'):
+        if h3.get('itemprop') == 'author':
+            author.append(h3.get_text())
+
+    # retrieve date
+    publi_date = soup.time.get_text()
+    publi_date = re.search(r"\d{2}/\d{2}/\d{4}", publi_date)[0]
+
+    content = ''
+    for p in soup.find_all('p'):
+        for p2 in re.finditer('py0p5', p.get('class')[-1]):
+            content += p.get_text()
+    content = unidecode.unidecode(content)
+
+    # retrieve theme
+    theme = ''
+    for meta in soup.find_all('meta'):
+        if meta.get('property') == 'og:url':
+            tmp = meta.get('content')[32:]
+            indice = tmp.find('/')
+            theme = tmp[:indice]
+
+    article = utils.recovery_article(title, 'FuturaSciences', author,
+                                     publi_date, content, theme)
+    return(article)
+
+
+def recovery_link_new_articles(url_rss):
+    """
+        Argument:
+            url_rss : string
+        Return:
+            retrieving links of new articles thanks to the rss feed
+    """
+    soup = utils.recovery_flux_urss(url_rss)
+    list_link = []
+    for link in soup.find_all("a"):
+        if link.get("class") == ["first-capitalize"]:
+            list_link.append("https://www.futura-sciences.com" +
+                             link.get("href"))
+    return(list_link)
+
+
+def recovery_new_articles_fusc(file_target = "data/clean/robot/" + str(date.datetime.now().date()) +"/"):
+    """
+        it create a json for each new article
+    """
+    links = recovery_link_new_articles("https://www.futura-sciences.com/" +
+                                       "flux-rss/")
+    list_articles = []
+    for article in links:
+        new_article = recovery_information_fusc(article)
+        list_articles.append(new_article)
+    utils.create_json(file_target, list_articles, "FuturaSciences/", "fusc")
+
+
+if __name__ == '__main__':
+    recovery_new_articles_fusc()
---
+++ server_ready_files/g4_liberation_V1.py
@@ -0,0 +1,145 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan  9 13:28:26 2018
+
+@author: Hbabou Nabil, Seguela Morgan
+
+V1.0 
+Extraction des articles du fux RSS
+
+V1.1
+Decoupage du code en Fonctions
+"""
+
+import os
+import lxml.html as lh
+import json
+import datetime as date
+from bs4 import BeautifulSoup
+import requests
+import re
+import unidecode
+import g4_utils_v31 as utilsg4
+import html
+
+
+def get_rss_infos():
+    """Get all articles link
+    
+    Returns:
+        list -- list of articles url
+    """
+
+    url_rss_lib = "http://www.liberation.fr/rss"
+    req = requests.get(url_rss_lib)
+    data = req.text
+    soup = BeautifulSoup(data, "lxml")
+
+    rss_items = soup.find_all("li")
+
+    rss_list = []
+
+    link_rss = []
+
+    for ri in rss_items:
+        if ri.get("class") == ['rss-item']:
+            rss_list.append(ri.a.get('href'))
+            
+    for rl in rss_list:
+        req = requests.get(rl)
+        data = req.text
+        soup = BeautifulSoup(data, "lxml")
+        entre = soup.find_all('entry')
+        for e in entre :
+            link_rss.append(e.link.get('href'))
+
+    return link_rss
+
+
+def get_information(article_link):
+    """Extact informations from all these articles
+    
+    Arguments:
+        article_link {string} -- url of an article
+    
+    Returns:
+        dict -- dict of all informations
+    """ 
+    
+    if "video" in article_link or "/apps/" in article_link or "checknews" in article_link or not re.search(r"\d\d\d\d/\d\d/\d\d", article_link):
+        return None
+
+    else:
+        
+        date_article = re.search(r"\d{4}/\d{2}/\d{2}", article_link)[0]
+        date_article = date.datetime.strptime(date_article, "%Y/%m/%d")
+        
+        diff_date = date.datetime.now() - date_article   
+        
+        if diff_date.days > 7:
+            return None
+
+        else: 
+            req = requests.get(article_link)
+            req.encoding = "utf-8"
+            data = req.text
+            soup = BeautifulSoup(data, "lxml")
+
+            if soup.find("div", class_ = "direct-headband") or article_link != req.url:
+                return None
+            else:               
+                balise_title = soup.find("h1")
+                balise_title = balise_title.get_text()
+                balise_title = re.sub(r"\s\s+", "", balise_title)
+
+                newspaper = "Liberation"
+                title = unidecode.unidecode(balise_title)
+
+                author = ""
+                for span in soup.find_all('span'):
+                    if span.get("class") == ['author']:
+                        if(span.a):
+                            author = span.a.string
+                    if span.get("class") == ['date']:
+                        if(span.time):
+                            date_p = date.datetime.strptime(span.time.get("datetime"),"%Y-%m-%dT%H:%M:%S").date()
+                            date_p = date_p.strftime("%d/%m/%Y")
+
+                content = ""        
+                for div in soup.find_all('div'):
+                    for p in div.find_all('p'):
+                        content += p.get_text() + " "
+                content = re.sub("<>", "", content)
+                content = unidecode.unidecode(content)
+                
+                new_article = utilsg4.recovery_article(title, newspaper, [author], date_p, content, " ")
+
+                return new_article
+
+def recuperation_info_libe(file_target = "data/clean/robot/" + str(date.datetime.now().date()) +"/"):
+    """Main fonction that get all articles url, extract the informations and create a JSON File
+    """
+
+    source = "liberation/"
+
+    link_rss = get_rss_infos()
+    
+    list_articles = []
+
+    for lr in link_rss:
+        if not "blogs.liberation" in lr:
+            informations = get_information(lr)
+        else:
+            informations = None
+        if informations:
+            list_articles.append(get_information(lr))
+    utilsg4.create_json(file_target, list_articles, source, "libe")
+
+    
+
+
+if __name__ == '__main__':
+    recuperation_info_libe()
+
+    
