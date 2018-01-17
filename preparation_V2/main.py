6b079f6f5963d5e5495f9bc373c10141e83faced
Etudiant <etudiant@debian>
2018-01-17 13:34:40 +0100

    creation de classe (version 2)

---
+++ preparation_V2/main.py
@@ -0,0 +1,20 @@
+from telerama import Telerama as Tera
+from minutes import Minutes as Minu
+from journal_exception import JournalException
+from journal_manager import JournalManager as JM
+import datetime as date
+from article import Article
+
+try:
+    #minu=Minu("20 minutes","minu","http://www.20minutes.fr")
+    tera=Tera("Telerama","tera","http://www.telerama.fr")
+    tera.add_article("se")
+    # ok print(len(tera.get_list_article()))
+    jm=JM(tera,"/home/etudiant/Documents/ProjetSID/preparation_v2/Art/" + str(date.datetime.now().date()) +"/","telerama")
+    # okart=Article("title","test",["Mr"],"12-1-2017","irem","cin")
+    # ok print(art.id_art)
+    # ok print(jm.already_exists(art))
+    #jm.create_json(new=False)
+
+except JournalException as je:
+    print(je)
