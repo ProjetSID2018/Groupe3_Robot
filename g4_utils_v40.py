63dcbf15f84d8280389516bab59b5ac86168bd27
AurelienPLT <35218076+AurelienPLT@users.noreply.github.com>
2018-01-18 13:21:49 +0100

    Modif fonction create_json
    Compter e nombre d'articles déjà existants

--- g4_utils_v40.py
+++ g4_utils_v40.py
@@ -118,12 +118,16 @@ def create_json(file_target, list_article, sources, abbreviation):
     """
     os.makedirs(file_target+sources, exist_ok=True)
     list_file = os.listdir(file_target+sources)
+    num_max = 0
     if list_file:
-        last_file = list_file[-1]
-        delimiter = last_file.split("_")
-        ii = int(delimiter[2]) + 1
-    else:
-        ii = 1
+        list_num = []
+        for file in list_file:
+            delimiter = file.split("_")
+            list_num.append(delimiter[2])
+        for num in list_num:
+            num_max = max(num_max, int(num))
+    ii = num_max + 1
+
     cur_date = date.datetime.now().date()
     for article in list_article:
         if not already_exists(article["date_publi"], article["title"],
@@ -157,8 +161,7 @@ def recovery_article(title, newspaper, authors, date_publi, content, theme):
         authors[ii] = unidecode.unidecode(authors[ii])
 
     content = re.sub(r"\s\s+", " ", content)
-    content = re.sub(r"\n", "", content)
-    content = re.sub(r"\r", "", content)
+    content = re.sub(r"\s", " ", content)
 
     new_article = {
                 "id_art": get_hash(date_publi, title, newspaper),
